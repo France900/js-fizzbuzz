5b9a245abad5c0f34fb3afb5bf54a3390781f9b8
France900 <francesco.em.livio@gmail.com>
2021-03-05 16:43:58 +0100

    ad ora le &%$@!

--- main.js
+++ main.js
@@ -3,9 +3,60 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-var numbers = []
+// var numbers = []
+//
+// for (var i = 0; i < 100; i++) {
+//   // console.log(i+1);
+//   numbers.push(i+1)
+//   document.getElementById("result").innerHTML += "<span>" + numbers[i] + " " + "</span>";
+//   if (!((i+1)%3) && !((i+1)%5)) {
+//     numbers[i] = "Fizz"
+//   } else if (!((i+1)%3)) {
+//     numbers[i] = "FizzBuzz"
+//   }else if (!((i+1)%5)) {
+//     numbers[i] = "Buzz"
+//   }
+// }
+// console.log(numbers);
+
+
+var numbers = [];
+var interference = numbers;
+console.log(numbers);
+console.log(interference);
+
 
 for (var i = 0; i < 100; i++) {
-  console.log(i+1);
-  document.getElementById("result").innerHTML += "<span>" + (i + 1) + "</span>";
+  // console.log(i+1);
+  numbers.push(i+1)
+  // var interference = numbers
+  if (!((i+1)%3) && !((i+1)%5)) {
+    interference[i] = "FizzBuzz"
+  } else if (!((i+1)%3)) {
+    interference[i] = "Fizz"
+  } else if (!((i+1)%5)) {
+    interference[i] = "Buzz"
+  }
+  document.getElementById("result").innerHTML += "<span>" + interference[i] + " " + "</span>";
+  // console.log(numbers[i]);
 }
+console.log(numbers);
+console.log(interference);
+
+
+
+// var numbers = []
+//
+// for (var i = 0; i < 100; i++) {
+//   console.log(i+1);
+//   numbers.push(i+1)
+//   if (!((i+1)%3) && !((i+1)%5)) {
+//     numbers[i] = "FizzBuzz"
+//   } else if (!((i+1)%3)) {
+//     numbers[i] = "Fizz"
+//   } else if (!((i+1)%5)) {
+//     numbers[i] = "Buzz"
+//   }
+//   document.getElementById("result").innerHTML += "<span>" + numbers[i] + " " + "</span>";
+// }
+// console.log(numbers);
