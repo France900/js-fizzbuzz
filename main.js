f51753945ea2f1be97b85a6d0fa2db0e087a7665
France900 <francesco.em.livio@gmail.com>
2021-03-05 17:34:32 +0100

    finito piu bello

--- main.js
+++ main.js
@@ -3,29 +3,13 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-// var numbers = []
-//
-// for (var i = 0; i < 100; i++) {
-//   numbers.push(i+1)
-//   document.getElementById("result").innerHTML += "<span>" + numbers[i] + " " + "</span>";
-//   if (!((i+1)%3) && !((i+1)%5)) {
-//     numbers[i] = "Fizz"
-//   } else if (!((i+1)%3)) {
-//     numbers[i] = "FizzBuzz"
-//   }else if (!((i+1)%5)) {
-//     numbers[i] = "Buzz"
-//   }
-// }
-// console.log(numbers);
-
-
 var numbers = [];
 var interference = []
 
 for (var i = 0; i < 100; i++) {
   numbers.push(i+1)
   interference.push(i+1);
-  // interference = numbers;
+  // interference = numbers; come diamine si fa la copia di backup della var?
   if (!((i+1)%3) && !((i+1)%5)) {
     interference[i] = "FizzBuzz"
   } else if (!((i+1)%3)) {
@@ -34,7 +18,7 @@ for (var i = 0; i < 100; i++) {
     interference[i] = "Buzz"
   }
   document.getElementById("result").innerHTML += "<span>" + interference[i] + " " + "</span>";
-  // console.log(numbers[i]);
 }
+
 console.log(numbers);
 console.log(interference);
